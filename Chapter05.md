41ca785aa50bb2df735b2a602db1bf5cf89a4901
blossun <dnsngu@gmail.com>
2020-10-12 16:40:58 +0900

    [Refactor]: Chapter05 5.2 RequestLine 파싱 코드를 별도의 메서드로 분리
    processRequestLine(String requestLine) 메서드로 분리
    요청라인을 GET, POST 메서드에 따라 method와 path, params(GET만)를 파싱
    
    issue #1

--- Chapter05.md
+++ Chapter05.md
@@ -20,3 +20,80 @@
 
 2장 문자열 계산기를 더 이상 리팩토링할 부분이 없다고 판단될 때까지 극단적으로 연습해 보는 것도 좋은 습관이다.
 
+
+
+RequestHandler 클래스의 책임을 분리한다. • RequestHandler 클래스는 많은 책임을 가지고 있다. 객체 지향 설계 원칙 중 “단일 책임의 원칙”에 따라RequestHandler 클래스가 가지고 있는 책임을 찾아 각 책임을 새로운 클래스를 만들어 분리한다.
+
+------
+
+- 힌트
+  - 클라이언트 요청 데이터를 담고 있는 InputStream을 생성자로 받아 HTTP 메소드, URL, 헤더, 본문을 분리하는 작업을 한다.
+  - 헤더는 Map<String, String>에 저장해 관리하고 getHeader("필드 이름") 메소드를 통해 접근 가능하도록 구현한다.
+  - GET과 POST 메소드에 따라 전달되는 인자를 Map<String, String>에 저장해 관리하고 getParameter("인자 이름") 메소드를 통해 접근 가능하도록 구현한다.
+  - RequestHandler가 새로 추가한 HttpRequest를 사용하도록 리팩토링한다.
+
+# 테스트 코드 구현
+
+위 요구사항을 구현하기 위해 새로운 클래스를 만들어 구현할 때 테스트 코드 기반으로 개발할 수 있다. `src/test/resources` 디렉토리에 `Http_GET.txt` 라는 이름으로 요청 데이터를 담고 있는 테스트 파일을 추가한다.
+
+※ `Http_GET.txt` 파일의 마지막 라인에 빈 공백 문자열을 포함해야 한다.
+
+이 파일을 이용한 테스트 코드 작성
+
+- Http_GET.txt
+
+```
+GET /user/create?userId=solar&password=1234&name=Sunyoung HTTP/1.1
+Host: localhost:8080
+Connection: keep-alive
+Accept: */*
+```
+
+- Http_POST.txt
+
+```
+GET /user/create HTTP/1.1
+Host: localhost:8080
+Connection: keep-alive
+Content-Length: 46
+Content-Type: application/x-www-form-urlencoded
+Accept: */*
+
+userId=solar&password=1234&name=Sunyoung
+```
+
+- 테스트 코드
+
+```java
+public class HttpRequestTest {
+    private String testDirectory = "./src/test/resources/";
+
+    @Test
+    public void request_GET() throws FileNotFoundException {
+        InputStream in = new FileInputStream(new File(testDirectory + "Http_GET.txt"));
+        HttpRequest request = new HttpRequest(in);
+
+        assertEquals("GET", request.getMethod());
+        assertEquals("/user/create", request.getPath());
+        assertEquals("keep-alive", request.getHeader("Connection"));
+        assertEquals("solar", request.getParameter("userId"));
+    }
+
+		@Test
+		public void request_POST() throws FileNotFoundException {
+		    InputStream in = new FileInputStream(new File(testDirectory + "Http_POST.txt"));
+		    HttpRequest request = new HttpRequest(in);
+		
+		    assertEquals("POST", request.getMethod());
+		    assertEquals("/user/create", request.getPath());
+		    assertEquals("keep-alive", request.getHeader("Connection"));
+		    assertEquals("solar", request.getParameter("userId"));
+		}
+}
+```
+
+테스트를 위한 HTTP 요청 데이터를 텍스트 파일에 생성 한 후, FileInputStream으로 읽은 후 이 InputStream을 새로 생성한 HttpRequest 클래스의 생성자로 전달하는 방식으로 테스트할 수 있다.
+
+위에 작성한 테스트 코드를 만족하는 HttpRequest 코드를 구현하면 된다.
+
+테스트 데이터를 위해 파이릉ㄹ 만들고 InputStream을 생성하는 과정이 번거롭게 느껴지면 String으로 문자열을 전달해 테스트할 수 있는 방법을 고민해보자
--- src/main/java/webserver/HttpRequest.java
+++ src/main/java/webserver/HttpRequest.java
@@ -5,22 +5,19 @@ import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
 import util.IOUtils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
 import static util.HttpRequestUtils.parseHeader;
 
 public class HttpRequest {
+    private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
+
     private String method;
     private String path;
     private Map<String, String> headers = new HashMap<>();
-    private Map<String, String> parameter = new HashMap<>();
-
-    private static final Logger log = LoggerFactory.getLogger(HttpRequest.class);
+    private Map<String, String> params = new HashMap<>();
 
     public String getMethod() {
         return method;
@@ -35,46 +32,54 @@ public class HttpRequest {
     }
 
     public String getParameter(String key) {
-        return parameter.get(key);
+        return params.get(key);
     }
 
-    public HttpRequest(InputStream in) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-        String line = br.readLine();
-        if (line == null) {
-            return;
+    public HttpRequest(InputStream in) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            String line = br.readLine();
+            if (line == null) {
+                return;
+            }
+
+            processRequestLine(line);
+
+            this.headers.put("Content-Length", "0");
+            line = br.readLine();
+            while (line != null && !line.equals("")) { //헤더 //line.equals("")만 있을 때, GET 메세지에서 NPE가 발생해서 조건 추가
+                log.debug("header : {}", line);
+                HttpRequestUtils.Pair header = parseHeader(line);
+                if (header != null) {
+                    headers.put(header.getKey(), header.getValue());
+                }
+                line = br.readLine();
+            }
+
+            if (this.method.equals("POST")) {
+                String body = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+                this.params = HttpRequestUtils.parseQueryString(body);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        log.debug("request line : {}", line);
-        String[] tokens = line.split(" ");
+    }
+
+    private void processRequestLine(String requestLine) {
+        log.debug("request line : {}", requestLine);
+        String[] tokens = requestLine.split(" ");
         this.method = tokens[0];
         // GET인우, POST인 경우
         if (this.method.equals("GET")) {
             this.path = parseDefaultUrl(tokens);
             if (path.contains("?")) {
                 this.path = parseDefaultUrl(tokens).substring(0, tokens[1].indexOf("?"));
-                this.parameter = HttpRequestUtils.parseQueryString(parseDefaultUrl(tokens).substring(tokens[1].indexOf("?") + 1));
+                this.params = HttpRequestUtils.parseQueryString(parseDefaultUrl(tokens).substring(tokens[1].indexOf("?") + 1));
             }
         } else {
             this.path = parseDefaultUrl(tokens);
         }
-
-        this.headers.put("Content-Length", "0");
-        line = br.readLine();
-        while (line != null && !line.equals("")) { //헤더 //line.equals("")만 있을 때, GET 메세지에서 NPE가 발생해서 조건 추가
-            log.debug("header : {}", line);
-            HttpRequestUtils.Pair header = parseHeader(line);
-            if (header != null) {
-                headers.put(header.getKey(), header.getValue());
-            }
-            line = br.readLine();
-        }
-
-        if (this.method.equals("POST")) {
-            String body = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
-            this.parameter = HttpRequestUtils.parseQueryString(body);
-        }
-
     }
 
     private String parseDefaultUrl(String[] tokens) {
