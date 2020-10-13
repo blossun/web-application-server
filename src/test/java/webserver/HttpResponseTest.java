5fe16008061d7b4cb8aaaac432a08e9842f6fb74
blossun <dnsngu@gmail.com>
2020-10-13 16:06:06 +0900

    [Test]: Chapter05 5.2 HttpResponse 테스트 코드 추가
    HttpResponseTest 코드는 HttpResponse를 통해 생성된 응답 데이터를 src/test/resources 에 파일을 생성해 수동으로 확인하도록 구현하고 있다. 수동으로 확인하는 번거로움이 있기는 하지만 HttpResponse를 다른 클래스에서 사용하기 전에 정상적으로 동작하는지 검증할 수 있다.
    * TODO : 수동 테스트를 개선하고 싶다면 `assertEquals()` 를 통해 자동화하도록 업데이트
    
    issue #2

--- src/test/java/webserver/HttpResponseTest.java
+++ src/test/java/webserver/HttpResponseTest.java
@@ -1,6 +1,5 @@
 package webserver;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 
 import java.io.File;
@@ -8,20 +7,19 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 
-public class HttpResponseTest extends TestCase {
+public class HttpResponseTest {
     private String testDirectory = "./src/test/resources/";
 
     @Test
     public void responseForward() throws Exception {
         // Http_Forward.txt 결과는 응답 body에 index.html이 포함되어 있어야 한다.
-        HttpResponse response = new HttpResponse(createOutputStream("Http_Redirect.txt"));
-        response.sendRedirect("/index.html");
-        assertEquals(1, 1);
+        HttpResponse response = new HttpResponse(createOutputStream("Http_Forward.txt"));
+        response.forward("/index.html");
     }
 
     @Test
     public void responseRedirect() throws Exception {
-        // Http_Redirect.txt 결과는 응답 headere에 Location 정보가 /index.html로 포함되어 있어야 한다.
+        // Http_Redirect.txt 결과는 응답 header에 Location 정보가 /index.html로 포함되어 있어야 한다.
         HttpResponse response = new HttpResponse(createOutputStream("Http_Redirect.txt"));
         response.sendRedirect("/index.html");
     }
@@ -29,7 +27,9 @@ public class HttpResponseTest extends TestCase {
     @Test
     public void responseCookies() throws Exception {
         // Http_Cookie.txt 결과는 응답 header에 Set-Cookie 값으로 logined=true 값이 포함되어 있 어야 한다.
-        HttpResponse response = new HttpResponse(createOutputStream("Http_Cookie.txt")); response.addHeader("Set-Cookie", "logined=true"); response.sendRedirect("/index.html");
+        HttpResponse response = new HttpResponse(createOutputStream("Http_Cookie.txt"));
+        response.addHeader("Set-Cookie", "logined=true");
+        response.sendRedirect("/index.html");
     }
 
     private OutputStream createOutputStream(String filename) throws FileNotFoundException {
