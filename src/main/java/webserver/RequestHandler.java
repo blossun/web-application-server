510eada1f76bd5764181f0a89ec630251117787a
blossun <dnsngu@gmail.com>
2020-10-13 17:53:52 +0900

    [Refactor]: Chapter05 5.3 클라이언트 요청 URL에 대한 분기 처리를 제거
    분기문에 해당하는 로직을 별도의 메소드로 추출
    리팩토링한 메소드는 HttpRequest, HttpResponse만 인자로 받고 있다. 이와 같이 메소드 원형이 같기 때문에 자바의 인터페이스로 추출하는 것이 가능
    
    issue #3

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -30,44 +30,56 @@ public class RequestHandler extends Thread {
 
             String url = request.getPath();
             if ("/user/create".equals(url)) {
-                User user = new User(request.getParameter("userId"),
-                        request.getParameter("password"),
-                        request.getParameter("name"),
-                        request.getParameter("email"));
-                log.debug("user : {}", user);
-                DataBase.addUser(user);
-                response.sendRedirect("/index.html");
+                createUser(request, response);
             } else if ("/user/login".equals(url)) {
-                User user = DataBase.findUserById(request.getParameter("userId"));
-                if (user != null && user.login(request.getParameter("password"))) {
-                    response.addHeader("Set-Cookie", "logined=true");
-                    response.sendRedirect("/index.html");
-                    return;
-                }
-                response.forward("/user/login_failed.html");
+                login(request, response);
             } else if ("/user/list".equals(url)) {
-                if (!request.isLogin()) {
-                    response.forward("/user/login.html");
-                    return;
-                }
-
-                Collection<User> users = DataBase.findAll();
-                StringBuilder sb = new StringBuilder();
-                sb.append("<table border='1'>");
-                for (User user : users) {
-                    sb.append("<tr>");
-                    sb.append("<td>" + user.getUserId() + "</td>");
-                    sb.append("<td>" + user.getName() + "</td>");
-                    sb.append("<td>" + user.getEmail() + "</td>");
-                    sb.append("</tr>");
-                }
-                sb.append("</table>");
-                response.forwardBody(sb.toString());
+                listUser(request, response);
             }
             response.forward(url);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void listUser(HttpRequest request, HttpResponse response) throws IOException {
+        if (!request.isLogin()) {
+            response.forward("/user/login.html");
+            return ;
+        }
+
+        Collection<User> users = DataBase.findAll();
+        StringBuilder sb = new StringBuilder();
+        sb.append("<table border='1'>");
+        for (User user : users) {
+            sb.append("<tr>");
+            sb.append("<td>" + user.getUserId() + "</td>");
+            sb.append("<td>" + user.getName() + "</td>");
+            sb.append("<td>" + user.getEmail() + "</td>");
+            sb.append("</tr>");
+        }
+        sb.append("</table>");
+        response.forwardBody(sb.toString());
+    }
+
+    private void login(HttpRequest request, HttpResponse response) throws IOException {
+        User user = DataBase.findUserById(request.getParameter("userId"));
+        if (user != null && user.login(request.getParameter("password"))) {
+            response.addHeader("Set-Cookie", "logined=true");
+            response.sendRedirect("/index.html");
+        } else {
+            response.forward("/user/login_failed.html");
+        }
+    }
+
+    private void createUser(HttpRequest request, HttpResponse response) {
+        User user = new User(request.getParameter("userId"),
+                request.getParameter("password"),
+                request.getParameter("name"),
+                request.getParameter("email"));
+        log.debug("user : {}", user);
+        DataBase.addUser(user);
+        response.sendRedirect("/index.html");
     }
 
 }
